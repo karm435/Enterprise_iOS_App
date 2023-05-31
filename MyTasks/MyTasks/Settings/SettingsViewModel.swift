c3c44d5ed3ef07ed1e2b315ddf6d05c2fbe78903
Karmjit <karmjit435@gmail.com>
2023-05-31 15:20:40 +1000

    Added more skeleton views and viewmodels

---
+++ MyTasks/MyTasks/Settings/SettingsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewModel.swift
+//  MyTasks
+//
+//  Created by karmjit singh on 31/5/2023.
+//
+
+import Foundation
--- MyTasks/MyTasks/TasksList/TasksListViewModel.swift
+++ MyTasks/MyTasks/TasksList/TasksListViewModel.swift
@@ -1,6 +1,14 @@
-
 import Foundation
+import Dependency
+import Models
+
 
 class TasksListViewModel: ObservableObject {
+	@Dependency(\.networkClient) var networkClient
+	
+	@Published var task: [Todo] = []
+	
+	public func onAppear() {
 		
+	}
 }
